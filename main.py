004639d806dc8c8cca5581de744a2a2cd82ba9ee
Алексей Полоз <polozhev@mail.ru>
2021-03-31 19:22:03 +0300

    Fix background task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import time
+from multiprocessing import Process
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -16,6 +17,8 @@ with open('sets.json', 'r') as file:
 with open('cities.json', 'r') as file:
 	CITIES = json.loads(file.read())
 
+LOCK = True
+
 
 app = FastAPI(title="Cities")
 cities = list(map(lambda i: i.lower(), CITIES))
@@ -24,6 +27,29 @@ cities = list(map(lambda i: i.lower(), CITIES))
 class CityInput(BaseModel):
 	city: str
 
+
+def background():
+	while True:
+		try:
+			cities_new = read(SHEET, 'Лист1', 1, 2, 1, 1000)
+		except Exception as e:
+			print('ERROR `read`', e)
+			time.sleep(1)
+			continue
+
+		try:
+			cities_new = [i[0] for i in cities_new if len(i)]
+		except Exception as e:
+			print('ERROR `convert` for {}'.format(cities_new), e)
+		else:
+			if cities_new != CITIES:
+				CITIES = cities_new
+				cities = list(map(lambda i: i.lower(), CITIES))
+
+				with open('cities.json', 'w') as file:
+					print(json.dumps(CITIES, ensure_ascii=False, indent='\t'), file=file)
+
+		time.sleep(600)
 
 def find(word):
 	word = word.lower()
@@ -45,27 +71,11 @@ def find(word):
 
 @app.post('/')
 async def add(req: CityInput):
+	global LOCK
+
+	if LOCK:
+		LOCK = False
+		process = Process(target=background)
+		process.start()
+
 	return find(req.city)
-
-
-while True:
-	try:
-		cities_new = read(SHEET, 'Лист1', 1, 2, 1, 1000)
-	except Exception as e:
-		print('ERROR `read`', e)
-		time.sleep(1)
-		continue
-
-	try:
-		cities_new = [i[0] for i in cities_new if len(i)]
-	except Exception as e:
-		print('ERROR `convert` for {}'.format(cities_new), e)
-	else:
-		if cities_new != CITIES:
-			CITIES = cities_new
-			cities = list(map(lambda i: i.lower(), CITIES))
-
-			with open('cities.json', 'w') as file:
-				print(json.dumps(CITIES, ensure_ascii=False, indent='\t'), file=file)
-
-	time.sleep(60)
